7a47ca17a0ccf2c00de38f9c7ff758168f5ecee2
huangminhang <mhesat@outlook.com>
2020-06-30 22:50:17 +0800

    update test

--- demo.py
+++ demo.py
@@ -7,30 +7,52 @@
 @File ： demo.py
 @IDE  ： PyCharm
 """
-
-
 from transitions import Machine
+from pprint import pprint
+
 
 class Matter(object):
     pass
 
 model = Matter()
 
-#The states argument defines the name of states
-states=['solid', 'liquid', 'gas', 'plasma']
+states=['solid', 'liquid', 'gas', 'plasma'] # 定义四种状态 : 固体 液体 气体 等离子体
 
-# The trigger argument defines the name of the new triggering method
+# trigger参数定义新触发方法的名称
 transitions = [
-    {'trigger': 'melt', 'source': 'solid', 'dest': 'liquid' },
-    {'trigger': 'evaporate', 'source': 'liquid', 'dest': 'gas'},
-    {'trigger': 'sublimate', 'source': 'solid', 'dest': 'gas'},
-    {'trigger': 'ionize', 'source': 'gas', 'dest': 'plasma'}]
+    {
+        'trigger': 'melt',
+        'source': 'solid',
+        'dest': 'liquid'
+    }, # 固体 融化 变成 液体
+    {
+        'trigger': 'evaporate',
+        'source': 'liquid',
+        'dest': 'gas'
+    }, # 液体 融化 变成 气体
+    {
+        'trigger': 'sublimate',
+        'source': 'solid',
+        'dest': 'gas'
+    }, # 固体 升华 变成 气体
+    {
+        'trigger': 'ionize',
+        'source': 'gas',
+        'dest': 'plasma'
+    }, # 气体 电离 变成 等离子体
+]
 
-machine = Machine(model=model, states=states, transitions=transitions, initial='solid')
+machine = Machine(
+    model=model,
+    states=states, # 状态
+    transitions=transitions, # 状态转化表
+    initial='solid' # 初始状态
+)
 
 # Test
-print(model.state)    # solid
-model.melt()
-print(model.state)   # liquid
-model.evaporate()
-print(model.state)
+# print(model.state)    # solid
+# model.melt()
+# print(model.state)   # liquid
+# model.evaporate()
+# print(model.state)
+pprint(dir(machine))
